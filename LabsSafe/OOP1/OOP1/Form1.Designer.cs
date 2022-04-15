982b84a3164d050b1986b4ea334d2fd08929d159
Volodymyr Kapeluh <42520850+LeaZerMe@users.noreply.github.com>
2022-04-15 11:44:13 +0300

    laba 2

---
+++ LabsSafe/OOP1/OOP1/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace OOP1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Name1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.course = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.group = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mark5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 226);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(92, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(136, 226);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(88, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(121, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name1,
+            this.course,
+            this.group,
+            this.mark5});
+            this.dataGridView1.Location = new System.Drawing.Point(26, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(455, 187);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(311, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 32);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Print";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(351, 226);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(28, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(244, 226);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(88, 20);
+            this.textBox5.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(453, 226);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(28, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(419, 226);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(28, 20);
+            this.textBox6.TabIndex = 9;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(385, 226);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(28, 20);
+            this.textBox7.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 281);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Total: 0";
+            // 
+            // Name1
+            // 
+            this.Name1.HeaderText = "name1";
+            this.Name1.Name = "Name1";
+            // 
+            // course
+            // 
+            this.course.HeaderText = "course";
+            this.course.Name = "course";
+            // 
+            // group
+            // 
+            this.group.HeaderText = "group";
+            this.group.Name = "group";
+            // 
+            // mark5
+            // 
+            this.mark5.HeaderText = "mark5";
+            this.mark5.Name = "mark5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 328);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn course;
+        private System.Windows.Forms.DataGridViewTextBoxColumn group;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mark5;
+    }
+}
+
---
+++ LabsSafe/OOP1/OOP1/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+
+namespace OOP1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            File.WriteAllText("C:\\Users\\sergey\\Desktop\\LabsSafe\\OOP1\\OOP1\\text.txt", "");
+            InitializeComponent();
+        }
+
+        int n = 0, dataLength = 0;
+   
+        ArrayList dataContainer = new ArrayList();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            dataContainer.Add(textBox1.Text);
+            dataContainer.Add(textBox2.Text);
+            dataContainer.Add(textBox5.Text);
+            dataContainer.Add(textBox4.Text);
+            dataContainer.Add(textBox7.Text);
+            dataContainer.Add(textBox6.Text);
+            dataContainer.Add(textBox3.Text);
+
+            WriteToFile();
+
+            label1.Text = "Total: " + ++dataLength;
+        }
+
+        private void WriteToFile()
+        {
+
+            using (StreamWriter writetext = File.AppendText("C:\\Users\\sergey\\Desktop\\LabsSafe\\OOP1\\OOP1\\text.txt"))
+            {
+                System.Diagnostics.Debug.WriteLine(dataContainer[0]);
+                writetext.WriteLine(dataContainer[0]);
+                writetext.WriteLine(dataContainer[1]);
+                writetext.WriteLine(dataContainer[2]);
+                writetext.WriteLine(dataContainer[3] + "," + dataContainer[4] + "," + dataContainer[5] + "," + dataContainer[6]);
+                writetext.WriteLine("//");
+            }
+            dataContainer.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ReadFile();
+        }
+
+        private void ReadFile()
+        {
+            List<string[]> extractedData = new List<string[]>();
+
+            using (StreamReader sr = File.OpenText("C:\\Users\\sergey\\Desktop\\LabsSafe\\OOP1\\OOP1\\text.txt"))
+            {
+                string newLine = "";
+                string[] userInfo = new string[4];
+                int userInfoIndex = 0;
+
+                while ((newLine = sr.ReadLine()) != null)
+                {
+
+                    if (newLine != "//")
+                    {
+                        userInfo[userInfoIndex++] = (newLine);
+                    } else
+                    {
+                        extractedData.Add(userInfo);
+                        userInfoIndex = 0;
+                    }
+                }
+
+                sr.Close();
+            }
+
+            Console.WriteLine(extractedData[1][0]);
+            FillData(extractedData);
+        }
+
+        private void FillData(List<string[]> data)
+        {
+            DataGridViewRow[] rowList = new DataGridViewRow[data.Count];
+
+            for (int i = 0; i < data.Count; i++)
+            {
+             
+                string[] currentUserInfo = (data[i]);
+                DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+
+                row.Cells[0].Value = currentUserInfo[0];
+                row.Cells[1].Value = currentUserInfo[1];
+                row.Cells[2].Value = currentUserInfo[2];
+                
+                string[] marks = currentUserInfo[3].Split(',');
+                int markFiveCounter = 0;
+
+                foreach (string five in marks)
+                {
+                   if(five == "5")
+                    {
+                        markFiveCounter++;
+                    }
+                }
+
+                row.Cells[3].Value = markFiveCounter.ToString();
+
+                rowList[i] = row;         
+            }
+
+            DataGridViewRow sortedData;
+
+            for (int i = 1; i < data.Count; i++)
+            {
+                int first = Int32.Parse(rowList[i].Cells[3].Value.ToString());
+                int second = Int32.Parse(rowList[i - 1].Cells[3].Value.ToString());
+
+                if (first < second)
+                {
+                    sortedData = rowList[i];
+                    rowList[i] = rowList[i - 1];
+                    rowList[i - 1] = sortedData;
+                }
+            }
+
+            dataGridView1.Rows.Clear();
+            for (int i = 0; i < data.Count; i++)
+            {
+                dataGridView1.Rows.Add(rowList[i]);
+            }
+
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+    }
+}
